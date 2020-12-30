52bc117ed6e3c5ab1abf693c6e0dfdaa3c64a271
Louis Rex <rlhardrock@gmail.com>
2020-12-29 21:44:24 -0500

    formas basicas de ruteado

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('bees', function () {
+    return "pagina web en blanco para renderizar abejas" ;
+});
+
+Route::get('bees/apiary_create', function () {
+    return "pagina con destino para crear ciber apiarios con formulario" ;
+});
+
+// variable, recibirla $ como parametro
+Route::get('bees/{specialty}', function ($specialty) {
+    return "pagina web en blanco para renderizar abejas $specialty" ;
+});
+
+//  ?  es opcional
+Route::get('bees/{specialty}/{weather?}', function ($specialty, $weather = null) {
+    if($weather){
+        return "pagina para distribucion $specialty de las abejas por $weather";
+    }else{
+        return "pagina para distribucion $specialty de las abejas";
+    }
+});
+
+
+
